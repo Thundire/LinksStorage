8c34356eef5b9b233c6f9a0a683c13cc1acecd0a
Tarxos <ftarxos@gmail.com>
2022-09-20 03:05:56 +0300

    add blank maui app

---
+++ LinksStorage/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LinksStorage
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ LinksStorage/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LinksStorage
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
