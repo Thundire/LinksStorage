1c63b934a70b7d86f1023178cb299b3524d1ee5a
Tarxos <ftarxos@gmail.com>
2022-09-22 19:30:04 +0300

    add maui community toolkit package

--- LinksStorage/MauiProgram.cs
+++ LinksStorage/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using LinksStorage.Pages;
+﻿using CommunityToolkit.Maui;
+using LinksStorage.Pages;
 using LinksStorage.ViewModels;
 
 namespace LinksStorage;
@@ -10,6 +11,7 @@ public static class MauiProgram
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
+            .UseMauiCommunityToolkit()
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
