46f849ed6e81af7a6eb4e99ff38bd01a044e7c9c
Tarxos <ftarxos@gmail.com>
2022-09-22 18:18:42 +0300

    add font awesome font icons to project

--- LinksStorage/MauiProgram.cs
+++ LinksStorage/MauiProgram.cs
@@ -14,6 +14,9 @@ public static class MauiProgram
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("Font Awesome 6 Brands-Regular-400.otf", "FABrand");
+                fonts.AddFont("Font Awesome 6 Free-Regular-400.otf", "FAFreeRegular");
+                fonts.AddFont("Font Awesome 6 Free-Solid-900.otf", "FAFreeSolid");
             });
 
         builder.Services
