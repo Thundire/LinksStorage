5c4cb699757edea840380006f946f3c2f6a2d97b
Tarxos <ftarxos@gmail.com>
2022-10-04 22:54:46 +0300

    add browser launcher service that will handle launching link in browser

---
+++ LinksStorage/Services/BrowserLauncherService.cs
@@ -0,0 +1,16 @@
+﻿namespace LinksStorage.Services;
+
+public class BrowserLauncherService
+{
+    public async ValueTask Open(string uri)
+    {
+        try
+        {
+            await Browser.OpenAsync(uri);
+        }
+        catch (Exception e)
+        {
+            
+        }
+    }
+}
