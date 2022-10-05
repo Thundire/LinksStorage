093668eb1a127ebd8caabb4acb69a553e23e6671
Tarxos <ftarxos@gmail.com>
2022-10-06 02:32:28 +0300

    fix icons sizing, and change edit icon

--- LinksStorage/Resources/Icons.cs
+++ LinksStorage/Resources/Icons.cs
@@ -6,7 +6,7 @@ public static class SolidIcons
     public const string Delete = "\uf1f8";
     public const string Link = "\uf0c1";
     public const string Star = "\u2b50";
-    public const string Edit = "\uf044";
+    public const string Edit = "\uf304";
     public const string Back = "\u2329";
     public const string House = "\ue3af";
 }
