ac4ac80f91d562b37f2baa607030b49eefdf2c71
Tarxos <ftarxos@gmail.com>
2022-10-06 01:10:33 +0300

    add view for links, rename links group view to group view and clear it code

--- LinksStorage/Controls/GroupView.xaml.cs
+++ LinksStorage/Controls/GroupView.xaml.cs
@@ -3,35 +3,35 @@ using LinksStorage.ViewModels;
 
 namespace LinksStorage.Controls;
 
-public partial class LinksGroup : ContentView
+public partial class GroupView : ContentView
 {
-	public LinksGroup() => InitializeComponent();
+	public GroupView() => InitializeComponent();
 
     public static readonly BindableProperty OpenCommandProperty = BindableProperty.Create(
         nameof(OpenCommand),
         typeof(ICommand),
-        typeof(LinksGroup),
+        typeof(GroupView),
         default(ICommand));
     public ICommand OpenCommand { get => (ICommand)GetValue(OpenCommandProperty); set => SetValue(OpenCommandProperty, value); }
 
     public static readonly BindableProperty GroupProperty = BindableProperty.Create(
         nameof(Group),
         typeof(GroupInfo),
-        typeof(LinksGroup),
+        typeof(GroupView),
         default(GroupInfo));
     public GroupInfo Group { get => (GroupInfo)GetValue(GroupProperty); set => SetValue(GroupProperty, value); }
 
     public static readonly BindableProperty ChangeNameCommandProperty = BindableProperty.Create(
         nameof(ChangeNameCommand),
         typeof(ICommand),
-        typeof(LinksGroup),
+        typeof(GroupView),
         default(ICommand));
     public ICommand ChangeNameCommand { get => (ICommand)GetValue(ChangeNameCommandProperty); set => SetValue(ChangeNameCommandProperty, value); }
 
     public static readonly BindableProperty RemoveCommandProperty = BindableProperty.Create(
         nameof(RemoveCommand),
         typeof(ICommand),
-        typeof(LinksGroup),
+        typeof(GroupView),
         default(ICommand));
     public ICommand RemoveCommand { get => (ICommand)GetValue(RemoveCommandProperty); set => SetValue(RemoveCommandProperty, value); }
 }
