db0978bc1b2c7ed0f852fa5d438b40213ab5c9c7
Tarxos <ftarxos@gmail.com>
2022-10-05 21:53:41 +0300

    add font icons as resources

---
+++ LinksStorage/Resources/Icons.cs
@@ -0,0 +1,17 @@
+﻿namespace LinksStorage.Resources;
+
+public static class SolidIcons
+{
+    public const string Group = "\uf07c";
+    public const string Delete = "\uf1f8";
+    public const string Link = "\uf0c1";
+    public const string Star = "\u2b50";
+    public const string Edit = "\uf044";
+    public const string Back = "\u2329";
+    public const string House = "\ue3af";
+}
+
+public static class RegularIcons
+{
+    public const string Star = "\u2b50";
+}
