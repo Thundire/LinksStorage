10b3a1066d19c078a47ec7b82987b6795e8496ab
Tarxos <ftarxos@gmail.com>
2023-08-05 22:45:43 +0300

    remove not needed models

---
+++ src/Data/Link.cs
@@ -0,0 +1,9 @@
+﻿namespace LinksStorage.Data;
+
+public class Link
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Uri { get; set; }
+    public bool Favorite { get; set; }
+}
